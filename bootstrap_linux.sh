0c4986202c38c54ffaf32609a9f03123270c148c
dongwa <783137663@qq.com>
2023-02-27 16:43:28 +0800

    chore(ubuntu): no need sudo

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# use sudo to run this sh
-
 # make in case they aren't already there
 mkdir -p "/usr/local/lib"
 mkdir -p "/usr/local/bin"
 
-apt-get update   
-apt-get install git curl
+sudo apt-get update   
+sudo apt-get install git curl
 
 # brew install git
 # Some git defaults
@@ -16,7 +14,7 @@ git config --global push.default simple
 
 # Install npm
 echo "Installing npm..."
-apt-get install npm
+sudo apt-get install npm
 
 # Install common use global Node modules
 modules=(
@@ -30,10 +28,10 @@ modules=(
 )
 
 echo "installing global node modules..."
-npm install -g ${modules[@]}
+sudo npm install -g ${modules[@]}
 
 # use n to install nodejs lts version 
-n lts
+sudo n lts
 
 # Centralize global npm packages for different node versions
 # echo "prefix = /usr/local" > ~/.npmrc
@@ -46,7 +44,7 @@ apps=(
 # Install apps to /Applications
 # Default is: /Users/$user/Applications
 echo "installing apps..."
-apt-get install ${apps[@]}
+sudo apt-get install ${apps[@]}
 
 # clone this repo
 git clone https://github.com/dongwa/system-preset ~/.dotfiles
